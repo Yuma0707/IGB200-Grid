3e5d92ea0f7e44a854ddd19ebad0bd46c6c9f8a1
JarvisH627 <h1120218040@gmail.com>
2024-10-30 16:01:52 +1000

    SFX FIX

--- Assets/Scripts/DraggableTile.cs
+++ Assets/Scripts/DraggableTile.cs
@@ -8,8 +8,13 @@ public class DraggableTile : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     public Vector3 startPosition;
     public Transform startParent;
     private CanvasGroup canvasGroup;
-    public TileSpawner tileSpawner;  // Tile Sponer Reference
+    public TileSpawner tileSpawner;  // Tile Spawner Reference
     public bool isDropped = false;  // Dropped or not
+
+    // Audio properties
+    public AudioClip pickUpSound;   // Sound effect for picking up
+    public AudioClip putDownSound;  // Sound effect for putting down
+    private AudioSource audioSource;  // AudioSource component
 
     // Properties of startParent
     public Transform StartParent
@@ -22,6 +27,13 @@ public class DraggableTile : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         canvasGroup = GetComponent<CanvasGroup>();
         startParent = transform.parent; // Save initial parent object
+
+        // Get or add AudioSource component
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();  // Add AudioSource component if it does not exist
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -29,11 +41,18 @@ public class DraggableTile : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         startPosition = transform.position; // Save the position at the start of the drag
         StartParent = transform.parent; // Save the parent at the start of the drag
         canvasGroup.blocksRaycasts = false; // Disable Raycast while dragging
+
+        // Play pick-up sound
+        if (pickUpSound != null)
+        {
+            audioSource.clip = pickUpSound;
+            audioSource.Play();
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // Converts mouse position to world coordinates
+        // Convert mouse position to world coordinates
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
         // Make tiles follow the mouse position
         transform.position = new Vector3(worldMousePos.x, worldMousePos.y, transform.position.z);
@@ -41,21 +60,28 @@ public class DraggableTile : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvasGroup.blocksRaycasts = true; // Return Raycast to active at the end of a drag
+        canvasGroup.blocksRaycasts = true; // Enable Raycast at the end of a drag
 
-        GameObject dropTarget = eventData.pointerEnter; // Obtain the object to drop to
+        GameObject dropTarget = eventData.pointerEnter; // Obtain the object to drop onto
 
         if (dropTarget != null && dropTarget.GetComponent<CustomDropTarget>() != null)
         {
             CustomDropTarget dropTargetComponent = dropTarget.GetComponent<CustomDropTarget>();
             dropTargetComponent.HandleDrop(this);
+
+            // Play put-down sound
+            if (putDownSound != null)
+            {
+                audioSource.clip = putDownSound;
+                audioSource.Play();
+            }
         }
         else
         {
-            // Restore original position if invalid drop destination
+            // Restore original position if the drop destination is invalid
             transform.position = startPosition;
             transform.SetParent(StartParent, false);
-            transform.localPosition = Vector3.zero; // Centered on the original panel
+            transform.localPosition = Vector3.zero; // Center on the original panel
         }
     }
 }
